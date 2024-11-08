46c06475c42350caa0adaf813c6a63549ed1ad5f
LeQuan128 <lequan0949213634@gmail.com>
2024-11-08 17:32:16 +0700

    Save changes on main before merging feature-UI

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    theme: 'dark', // Giao diện tối
-    itemsPerPage: 10
-    databaseURL: 'http://localhost:27017', // URL của cơ sở dữ liệu
- allowAssignment: true, // Bật tính năng gán công việc
+    theme: 'dark', 
+    itemsPerPage: 20
+    databaseURL: 'http://localhost:27017', 
+    allowAssignment: true, 
 };
